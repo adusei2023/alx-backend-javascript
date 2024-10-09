554c5b8e1fee1c4a65cc4445cce1f0a89832b5de
Samuel Adusei Boateng <134881036+adusei2023@users.noreply.github.com>
2024-10-09 22:45:54 +0000

    Update main.ts

---
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -0,0 +1,29 @@
+// task_5/js/main.ts
+
+// Define MajorCredits interface
+interface MajorCredits {
+    credits: number;
+    brand: 'Major';
+}
+
+// Define MinorCredits interface
+interface MinorCredits {
+    credits: number;
+    brand: 'Minor';
+}
+
+// Function to sum MajorCredits
+function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
+    return {
+        credits: subject1.credits + subject2.credits,
+        brand: 'Major'
+    };
+}
+
+// Function to sum MinorCredits
+function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
+    return {
+        credits: subject1.credits + subject2.credits,
+        brand: 'Minor'
+    };
+}
