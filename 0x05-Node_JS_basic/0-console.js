509350221040de3cdba684c4062b441b8ce7c3a6
adusei2023 <samuel.boateng01@yahoo.com>
2024-11-26 19:57:37 +0000

    done

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Samuel Boateng <https://github.com/adusei2023>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
